96ba1ed9f72bb03d456907067ee0283c5491afb8
manbaxz123 <manvmse130081@fpt.edu.vn>
2020-11-09 17:57:39 +0700

    deployment change

--- src/main/java/capstone/openjob/oauth2/controller/LoginController.java
+++ src/main/java/capstone/openjob/oauth2/controller/LoginController.java
@@ -20,19 +20,19 @@ public class LoginController {
     HttpHeaders httpHeaders = new HttpHeaders();
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    @ResponseBody
-    @CrossOrigin(origins = "http://localhost:8080")
+//    @ResponseBody
+//    @CrossOrigin(origins = "http://localhost:8080")
     ResponseEntity<String>  login(@RequestParam("username") String email, @RequestParam("password") String password) throws Exception {
-        String url = "http://localhost:8080/oauth/token";
-//        String url = "https://oras-api.herokuapp.com/oauth/token";
+//        String url = "http://localhost:8080/oauth/token";
+        String url = "https://openjob-server.herokuapp.com/oauth/token";
         URL obj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-//        HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
+//        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+        HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
         connection.setRequestMethod(RequestMethod.POST.name());
         connection.addRequestProperty("User-Agent",
                 "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-        connection.setRequestProperty("Host", "localhost:8080");
-//        connection.setRequestProperty("Host", "oras-api.herokuapp.com");
+//        connection.setRequestProperty("Host", "localhost:8080");
+        connection.setRequestProperty("Host", "openjob-server.herokuapp.com");
         connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
         connection.setRequestProperty("Authorization", "Basic bXktdHJ1c3RlZC1jbGllbnQ6c2VjcmV0");
         String urlParameters = "grant_type=password&username="+ email + "&password=" + password;
