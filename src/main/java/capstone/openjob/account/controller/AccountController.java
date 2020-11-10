29e0e6de94042f8ba3df0ce8527be7e8c1a00ddb
leplep190599 <59325529+leplep190599@users.noreply.github.com>
2020-11-11 03:08:36 +0700

    t buồn ngủ quá không bk ghi gì

--- src/main/java/capstone/openjob/account/controller/AccountController.java
+++ src/main/java/capstone/openjob/account/controller/AccountController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/v1/account-mananagement")
+@CrossOrigin(value = "http://localhost:4200")
 public class AccountController {
 
     @Autowired
